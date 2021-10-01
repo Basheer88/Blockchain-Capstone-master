ff573c1c747815729ab024386a9c3e8bc094d7fc
Basheer AbdulMutalb Hassoon <basherbgw@gmail.com>
2021-10-01 17:06:31 +0300

    Update ERC721Mintable.sol

--- eth-contracts/contracts/ERC721Mintable.sol
+++ eth-contracts/contracts/ERC721Mintable.sol
@@ -572,7 +572,7 @@ contract ERC721Metadata is ERC721Enumerable, usingOraclize {
 //      -returns a true boolean upon completion of the function
 //      -calls the superclass mint and setTokenURI functions
 
-contract CustomERC721Token is ERC721Metadata {
+contract CustomERC721Token is ERC721Metadata("firstToken","FT","https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/") {
 
     function mint(address to, uint256 tokenID, string tokenURI) public onlyOwner returns(bool){
         super._mint(to, tokenId);
@@ -580,4 +580,3 @@ contract CustomERC721Token is ERC721Metadata {
         return true;
     }
 }
-
