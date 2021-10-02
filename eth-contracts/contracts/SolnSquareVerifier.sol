5af48b1c8f06fd72e57e23e9a60e3c6ff71369fa
Basheer AbdulMutalb Hassoon <basherbgw@gmail.com>
2021-10-02 13:06:02 +0300

    Update SolnSquareVerifier.sol

--- eth-contracts/contracts/SolnSquareVerifier.sol
+++ eth-contracts/contracts/SolnSquareVerifier.sol
@@ -13,28 +13,42 @@ contract SolnSquareVerifier is CustomERC721Token {
         verifierContract = verifier(verifierAddress);
     }
 
-// TODO define a solutions struct that can hold an index & an address
+    // TODO define a solutions struct that can hold an index & an address
+    struct solutions {
+        uint256 index;
+        address add;
+    }
+
+    // TODO define an array of the above struct
+    solutions[] private sol;
+
+    // solutions array counter 
+    uint256 counter = 0;
+
+    // TODO define a mapping to store unique solutions submitted
+    mapping(address => solutions ) private store;
 
 
-// TODO define an array of the above struct
+    // TODO Create an event to emit when a solution is added
+    event Added(uint256 solutionsIndex, address solutionsAdd);
 
 
-// TODO define a mapping to store unique solutions submitted
+    // TODO Create a function to add the solutions to the array and emit the event
+    function add(uint256 recievedIndex, address recievedAddress) public {
+        sol[counter] = solutions(recievedIndex, recievedAddress);
+        store[msg.sender] = solutions(recievedIndex, recievedAddress);
+        counter++;
+        emit Added(recievedIndex, recievedAddress);
+    }
 
 
-
-// TODO Create an event to emit when a solution is added
-
-
-
-// TODO Create a function to add the solutions to the array and emit the event
+    // TODO Create a function to mint new NFT only after the solution has been verified
+    //  - make sure the solution is unique (has not been used before)
+    //  - make sure you handle metadata as well as tokenSuplly
+    function mint(string name, string symbol) public {
 
 
-
-// TODO Create a function to mint new NFT only after the solution has been verified
-//  - make sure the solution is unique (has not been used before)
-//  - make sure you handle metadata as well as tokenSuplly
-
+    }
   
 
 }
