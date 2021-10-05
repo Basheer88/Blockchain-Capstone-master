2687b2d372f57af23992c47a787751514f4338b0
Basheer AbdulMutalb Hassoon <basherbgw@gmail.com>
2021-10-05 19:45:27 +0300

    update

--- eth-contracts/test/TestSquareVerifier.js
+++ eth-contracts/test/TestSquareVerifier.js
@@ -9,10 +9,10 @@ contract('TestSquareVerifier', accounts => {
 
     // - use the contents from proof.json generated from zokrates steps
     let proof = {
-        a: ["0x000cec419d581bca9684c23f54d63a1a208a6a8248199d8b8e7e38a9ea464fc2", "0x012178abe0cb39e4641b020cbf4a12710b8205a8c440b76ae3c7307b243fab59"],
-        b: [["0x06ccf3d807b74e26ee0c4105f4d58567c3562596d8c67e0322cb3119492bf937", "0x226ab2d85961362a826769e08b22a4d339b6a9c11b7bc81e604ce03bcf2c7d95"], ["0x2c0bd3d77ba722537b35472d54eae0e4af0a0bc5d53ac7fe92eff3e78c30cbd8", "0x29b6923b0a56c9157c9d1ff07416c2b1690ca871cfaedf28ccf337efad79e12c"]],
-        c: ["0x1c676558b327c895034fc50ac99ef1ff02d15e5c84a05f06afb90e95345d5e53", "0x26c0fa3a76ee257931e73d67b83d0b8840df70d54628dda31af57cd61720126e"],
-        input: ["0x0000000000000000000000000000000000000000000000000000000000000009", "0x0000000000000000000000000000000000000000000000000000000000000001"]
+        a: ["0x04c288a27acf246e10741c1ab8b962ae094c4231a0fb3f332bd762ece7fd60d3", "0x2c6825c447b3bad7e95b091c1fb974a060babe1aae6b4f970e2a72ea7d2b6be1"],
+        b: [["0x0fb4556fbf79693baec9291a19d53b0ccaac33b47d7dbb7c5cd050a6e765d438", "0x20bcc0658406bfb7c8c8b12fad8c4e4d4745355ff94fa491648d817a36f18d5b"], ["0x185c9d0b7b0848e0fbb0629eafdb309e45a521433931ae9ec36c590c78a386ac", "0x0114ee182c9b47a0089a2f34b74f33f64be3cffdecc37d97ebd3d02185e4bb28"]],
+        c: ["0x16b449b38b144948bdbcd90c19b5aac20323cb276c038d4b28214c53d3416897", "0x20d9bca96481dd62c3b168c302476f2acf2d959c8b21af42410305c1d5123dc6"],
+        inputs: ["0x0000000000000000000000000000000000000000000000000000000000000009", "0x0000000000000000000000000000000000000000000000000000000000000001"]
     }
 /*
     // Before for each
@@ -23,7 +23,7 @@ contract('TestSquareVerifier', accounts => {
     // Test verification with correct proof
     it('correct proof verification test', async function () {
         let instance = await Verifier.deployed();
-        result = await instance.verifyTx.call(proof.a, proof.b, proof.c, proof.input, {from: owner});
+        result = await instance.verifyTx.call(proof.a, proof.b, proof.c, proof.inputs, {from: owner});
         assert.equal(result, true, "Proofing with correct proof Failed");
     })
         
